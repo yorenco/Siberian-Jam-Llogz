d1e1452bf4a9e155a796b9563231c04570a3743f
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-11-02 10:59:57 +0300

    add sound trigger

---
+++ Assets/Scripts/Trigger/AudioTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioTrigger : BaseTrigger
+{
+    [SerializeField] private AudioClip[] _clips;
+    private AudioSource _audio;
+
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    public override void Activate()
+    {
+        if (_clips == null || _clips.Length == 0) return;
+
+        AudioClip clip = _clips[Random.Range(0, _clips.Length)];
+        _audio.PlayOneShot(clip);
+    }
+}
